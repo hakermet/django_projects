98df271548de8299017dd7f7a614c4d561c85a55
Lenovo <aleksandr.kruhlikov@gmail.com>
2024-10-08 13:36:58 +0300

    m

---
+++ kruhlikov_sample_project/hello_app/views.py
@@ -0,0 +1,36 @@
+from django.http import HttpResponse
+
+
+class View:
+    @staticmethod
+    def hello(request) -> HttpResponse:
+        try:
+            count_reload_page: int = request.session["count_reload_page"]
+            count_reload_page += 1
+            all_cookies: str = request.COOKIES
+            request.session["count_reload_page"]: int = count_reload_page
+            response: HttpResponse = HttpResponse(f'<div style="width: 100%; height: 100%; text-align: center; '
+                                    f'background-color: #303030;"><h1 '
+                                    f'style="color: white">Hello, '
+                                    f'Sasha!</h1> <br><strong style="color: white; position: absolute; bottom: 20px; margin-left: -270px;'
+                                    f'">All cookies: {all_cookies} <br>'
+                                    f'Count reload '
+                                    f'pag'
+                                    f'e: {count_reload_page} </strong>'
+                                    f"</div>")
+            response.set_cookie("author", "Kruhlikov")
+            return response
+        except:
+            request.session["count_reload_page"]: int = 0
+            count_reload_page = 0
+            all_cookies: str = request.COOKIES
+            response: HttpResponse = HttpResponse(f'<div style="width: 100%; height: 100%; text-align: center; '
+                                                  f'background-color: #303030;"><h1 '
+                                                  f'style="color: white">Hello, '
+                                                  f'Sasha!</h1> <br><strong style="color: white; position: absolute; bottom: 20px; margin-left: -270px;'
+                                                  f'">All cookies: {all_cookies} <br>'
+                                                  f'Count reload '
+                                                  f'pag'
+                                                  f'e: {count_reload_page} </strong>'
+                                                  f"</div>")
+            return response
---
+++ kruhlikov_sample_project/home_app/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+class View:
+    @staticmethod
+    def home(request):
+        return render(request, 'home_app/home.html')
