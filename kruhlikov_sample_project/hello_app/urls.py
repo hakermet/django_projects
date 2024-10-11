46ea7e7dd028b990ac86a48b69b2874655055954
Lenovo <aleksandr.kruhlikov@gmail.com>
2024-10-11 13:24:12 +0300

    fixed code

--- kruhlikov_sample_project/hello_app/urls.py
+++ kruhlikov_sample_project/hello_app/urls.py
@@ -2,5 +2,5 @@ from .views import View
 from django.urls import path
 
 urlpatterns = [
-    path("", View.hello, name="hello")
+    path("", View.hello, name="hello"),
 ]
--- kruhlikov_sample_project/hello_app/views.py
+++ kruhlikov_sample_project/hello_app/views.py
@@ -1,6 +1,5 @@
 from django.http import HttpResponse
 
-
 class View:
     @staticmethod
     def hello(request) -> HttpResponse:
